6dea4a29eb06e4080ae8d08fa9b2ff128c200411
Alex J <jones6211@gmail.com>
2019-01-16 21:12:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#API Timestamp project
+
+1. API endpoint: GET https://znematoadz.github.io/timestamp/api/:date_string?
+2. If :data_string? is valid the api returns JSON with both UNIX and UTC date forms.
+3. If :data_string? is empty the api returns current date and time (GMT)
+4. If :data_string? is invalid the api returns {error: "Invalid Date"}
+
+###Example
+
+[https://znematoadz.github.io/timestamp/api/2019-12-25]
+
+[https://znematoadz.github.io/timestamp/api/1577232000000]
---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+
+let app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
+
+app.get("/api/:date_string?", (req, res, next) => {
+  let date;
+  if (req.params.date_string !== undefined) {
+    let unix = parseInt(req.params.date_string * 1);
+    if (isNaN(unix)) {
+      date = new Date(req.params.date_string);
+    } else {
+      date = new Date(unix);
+    }
+  } else {
+    date = new Date(Date.now());
+  }
+
+  let data =
+    date == "Invalid Date"
+      ? { error: "Invalid Date" }
+      : { unix: date.getTime(), utc: date.toUTCString() };
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("serving..."));
